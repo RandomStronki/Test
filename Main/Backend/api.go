d59199b53adb2e9b72bbba485deb45a9c4ea5963
Adrian-Dz <dzieladrian@gmail.com>
2025-03-24 22:32:53 +0100

    Frontend

--- Main/Backend/api.go
+++ Main/Backend/api.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func PostGame(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetGame(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	router := chi.NewRouter()
+
+	router.HandleFunc("/game/post/{}", PostGame)
+	router.HandleFunc("/player/", GetGame)
+
+	fmt.Println("Listening on port 8080")
+	http.ListenAndServe(":8080", router)
+}
